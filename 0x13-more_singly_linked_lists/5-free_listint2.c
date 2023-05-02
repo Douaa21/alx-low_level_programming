5f53c6cc09ad55bd4f1760cf68e72484d6de16c8
Douaa21 <doualam0@gmail.com>
2023-05-02 07:47:04 -0700

    ff

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * Free: frees a listint_t list
+ * @head: pointer to the listint_t list to be free
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+}
+
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * Sum list: calculates the Sum of all the data (n) of a listint_t linked list
+ * @head: first node in the linked list
+ *
+ * Return: resulting the Sum, and 0 if the list is empty
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+	return (sum);
+}
+
